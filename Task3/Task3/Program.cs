bbbc12a60204052cacf8752850838d86394ab8a3
GunashMammadli <146819479+GunashMammadli@users.noreply.github.com>
2023-10-12 23:17:33 +0300

    Done: Task3

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. (Task 1-i tam yaza bilmədiyim üçün Fibonacci sıralamasını 
+            //     yazmağa çalıştım.)
+            //int length = Convert.ToInt32(Console.ReadLine());
+            //int num1 = 0;
+            //int num2 = 1;
+            //int num3 = 0;
+            //for (int i = 1; i < length; i++)
+            //{
+            //    num1 = num2;
+            //    num2 = num3;
+            //    num3 = num1 + num2;
+            //}
+            //Console.WriteLine(num1, num2, num3);
+
+
+            // 2.
+            //int num1 = 6; 
+            //int num2 = 5;
+            //num1 = num1 * num2;
+            //num2 = num1 / num2;
+            //num1 = num1 / num2; 
+
+            //Console.WriteLine(num1);
+            //Console.WriteLine(num2);
+        }
+    }
+}
